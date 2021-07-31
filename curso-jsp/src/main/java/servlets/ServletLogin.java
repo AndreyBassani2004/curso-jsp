9c722f454732a8890246e53b018a1c833910f5f9
Usuario-2021 <Usuario-2021@LAPTOP-VD13A7ED>
2021-07-31 11:48:34 -0300

    Criando DAO, login com BD

--- curso-jsp/src/main/java/servlets/ServletLogin.java
+++ curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -9,61 +9,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.DAOLoginRepository;
 import model.ModelLogin;
 
-
-@WebServlet(urlPatterns = {"/principal/ServletLogin", "/ServletLogin"})
+@WebServlet(urlPatterns = { "/principal/ServletLogin", "/ServletLogin" })
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-   
-    public ServletLogin() {
-       
-    }
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public ServletLogin() {
+
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
 		String url = request.getParameter("url");
-		
-		if(login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
-			
-			ModelLogin modelLogin = new ModelLogin();
-			modelLogin.setLogin(login);
-			modelLogin.setSenha(senha);
-			
-			if(modelLogin.getLogin().equalsIgnoreCase("admin") && modelLogin.getSenha().equalsIgnoreCase("admin")) {
-				
-				request.getSession().setAttribute("usuario", modelLogin.getLogin());
-				
-				if(url == null || url.equals("null")) {
-					url = "principal/principal.jsp";
-					
+
+		try {
+
+			if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+
+				ModelLogin modelLogin = new ModelLogin();
+				modelLogin.setLogin(login);
+				modelLogin.setSenha(senha);
+
+				if (daoLoginRepository.validarAutenticacao(modelLogin)) {
+
+					request.getSession().setAttribute("usuario", modelLogin.getLogin());
+
+					if (url == null || url.equals("null")) {
+						url = "principal/principal.jsp";
+
+					}
+
+					RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+					redirecionar.forward(request, response);
+				} else {
+					RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+					request.setAttribute("msg", "Senha ou Login incorreto");
+					redirecionar.forward(request, response);
+
 				}
-				
-				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+
+			} else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Preencha todos os campos");
 				redirecionar.forward(request, response);
-			}else {
-				RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
-				request.setAttribute("msg", "Senha ou Login incorreto");
-				redirecionar.forward(request, response);
-				
 			}
-			
-		}else {
-			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
-			request.setAttribute("msg", "Preencha todos os campos");
-			redirecionar.forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		doGet(request, response);
 	}
 
 }
