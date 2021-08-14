2e99e1b5dda8854aa87ad6f1112c11571f2d2b5f
AndreyBassani2004 <andreyjoseab@outlook.com>
2021-08-14 08:26:11 -0300

    teste

--- curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
+++ curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
@@ -52,7 +52,7 @@ public class DAOUsuarioRepository {
 		return modelLogin;
 
 	}
-
+//
 	public boolean validarLogin(String login) throws Exception {
 		String sql = "select count(1) > 0 as existe from model_login where upper(login) = upper('"+login+"');";
 		PreparedStatement statement = connection.prepareStatement(sql);
