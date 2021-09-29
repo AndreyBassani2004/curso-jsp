4f4850bcda3f2054e9b59ec2af90ccbf2f8b4628
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2021-09-29 09:52:16 -0300

    Criando nosso ReportUtil

---
+++ curso-jsp/src/main/java/util/ReportUtil.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+public class ReportUtil implements Serializable{
+	
+	private static final long serialVersionUID = 1L; 
+ 
+	
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public byte[] geraRelatorioPDF(List listaDados,String nomeRelatorio, ServletContext servletContext) throws Exception{
+		
+		JRBeanCollectionDataSource jrbcds = new JRBeanCollectionDataSource(listaDados);
+		
+		String caminhoJasper = servletContext.getRealPath("relatorio") + File.separator + nomeRelatorio + ".jasper";
+		
+	
+		JasperPrint impressoraJasper = JasperFillManager.fillReport(caminhoJasper, new HashMap(), jrbcds);
+		
+		return JasperExportManager.exportReportToPdf(impressoraJasper);
+	}
+}
+
